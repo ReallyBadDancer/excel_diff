70a5229b8dccc8edb2569443f00745dd24818999
Jason Soares <jason.soares@viasat.com>
2024-07-06 10:44:38 -0700

    Fixed a logic error where the delete case was trying to show text from new string instead of old string in strikeout format.

--- diff_writer.py
+++ diff_writer.py
@@ -1,8 +1,9 @@
 import xlsxwriter
+from xlsxwriter.worksheet import Format
 from difflib import SequenceMatcher
 
 
-def output_excel_formatting(orig: str, new: str, f_uline, f_sout, f_norm, wr) -> list:
+def output_excel_formatting(orig: str, new: str, f_uline: Format, f_sout: Format, f_norm: Format, wr: Format) -> list:
     # Matcher gets a sequence of "opcodes" which say to insert, delete, etc. blocks of text.
     matcher = SequenceMatcher(None, orig, new)
     opcodes = matcher.get_opcodes()
@@ -17,7 +18,7 @@ def output_excel_formatting(orig: str, new: str, f_uline, f_sout, f_norm, wr) ->
             result.append("".join(new[j1:j2]))
         elif tag == 'delete':
             result.append(f_sout)
-            result.append("".join(new[j1:j2]))
+            result.append("".join(orig[i1:i2]))
         elif tag == 'replace':
             result.append(f_sout)
             result.append("".join(orig[i1:i2]))
@@ -28,25 +29,46 @@ def output_excel_formatting(orig: str, new: str, f_uline, f_sout, f_norm, wr) ->
     return result
 
 
-def output_to_excel_worksheet(s1, s2, col, row):
+def output_to_excel_worksheet(diff_list: list) -> None:
     # Open a workbook and choose the column to format
     workbook = xlsxwriter.Workbook('rich_strings.xlsx')
     worksheet = workbook.add_worksheet()
-    worksheet.set_column(first_col=col, last_col=col, width=100)
+    worksheet.set_column(first_col=0, last_col=2, width=40)
     # Set up some formats to use.
     normal = workbook.add_format()
     underline = workbook.add_format({'underline': True, 'font_color': 'green'})
     strikeout = workbook.add_format({'font_strikeout': True, 'font_color': 'red'})
     wrap = workbook.add_format()
     wrap.set_text_wrap()
-    # Generate the diff in xlsxwriter output format.
-    excel_data = output_excel_formatting(s1, s2, underline, strikeout, normal, wrap)
-    # Write the xlsxwriter data to the target cell and close workbook
-    worksheet.write_rich_string(row, col, *excel_data)
+
+    for (s1, s2), row in zip(diff_list, range(len(diff_list))):
+        worksheet.write(row, 0, s1)
+        worksheet.write(row, 1, s2)
+        diff = output_excel_formatting(s1, s2, underline, strikeout, normal, wrap)
+        worksheet.write_rich_string(row, 2, *diff)
+
+    # worksheet.write_rich_string(0, 2, *excel_data)
     workbook.close()
 
 
 if __name__ == '__main__':
     string_a = "The quick brown fox jumped over the lazy dog"
     string_b = "The slow brown fox walked over a lazy cat"
-    output_to_excel_worksheet(string_a, string_b, 0, 0)
+    string_c = "How now brown cow"
+    string_d = "How then brown hen"
+    output_to_excel_worksheet([[string_c, string_d], [string_a, string_b]])
+
+    # Step by step test
+    # workbook = xlsxwriter.Workbook('xlsxwriter_test.xlsx')
+    # worksheet = workbook.add_worksheet()
+    # worksheet.set_column(first_col=0, last_col=2, width=30)
+    # # Set up some formats to use.
+    # normal = workbook.add_format()
+    # underline = workbook.add_format({'underline': True, 'font_color': 'green'})
+    # strikeout = workbook.add_format({'font_strikeout': True, 'font_color': 'red'})
+    # wrap = workbook.add_format()
+    # wrap.set_text_wrap()
+    # format_list = [normal, "Test", underline, " Underline"]
+    # worksheet.write_rich_string(0, 0, *format_list)
+    # workbook.close()
+
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from openpyxl import load_workbook
 from redlines import Redlines
 from diff_writer import output_to_excel_worksheet
+from pprint import pp
 
 wb = load_workbook("test.xlsx")
 ws = wb.active
 
-with open('asdf.txt', mode='w') as ofile:
-    ofile.write("|Orig Value|New Value|Diff|\n|-|-|-|\n")
+row_list = []
+for row in ws.values:
+    value_list = []
+    for cell in row:
+        # value_list.append(cell.replace("\n", "<br>"))
+        value_list.append(cell)
+    row_list.append(value_list)
 
-    for row in ws.values:
-        value_list = []
-        for cell in row:
-            # value_list.append(cell.replace("\n", "<br>"))
-            value_list.append(cell)
+pp(row_list)
 
-        test = Redlines(value_list[0], value_list[1], markdown_style="none")
-        ofile.write(f"|{value_list[0]}|{value_list[1]}|{test.output_markdown}|\n")
-        print(f"|{value_list[0]}|{value_list[1]}|{test.output_markdown}|\n")
+output_to_excel_worksheet(row_list)
 
 
 # Step 1: Import the workbook in OpenPyxl with the test text and save the contents as a dictionary.
