dd94aee36194d1589f4c7573ccf9abc727198b4a
Jason Soares <jason.soares@viasat.com>
2024-07-11 14:24:24 -0700

    Added cases to handle brand new, unchanged, or entirely deleted items to output_excel_formatting() Updated styles to all include text wrapping and deleted appended wrap Format object on the end of the diff list output in output_excel_formatting() Added some user input string handling to main. Removed some unnecessary prints from main. Added additional test cases to test.xlsx.

--- diff_writer.py
+++ diff_writer.py
@@ -3,7 +3,15 @@ from xlsxwriter.worksheet import Format
 from difflib import SequenceMatcher
 
 
-def output_excel_formatting(orig: str, new: str, f_uline: Format, f_stkout: Format, f_norm: Format, wr: Format) -> list:
+def output_excel_formatting(orig: str, new: str, f_uline: Format, f_stkout: Format, f_norm: Format) -> list:
+
+    if orig is None:  # New item that didn't exist in orig. Use underline style.
+        return [f_uline, new]
+    elif orig == new:  # No change since orig, use normal style.
+        return [f_norm, orig]
+    elif new is None:  # Completely deleted orig, use strikeout style.
+        return [f_stkout, orig]
+
     # Matcher gets a sequence of "opcodes" which say to insert, delete, etc. blocks of text.
     matcher = SequenceMatcher(None, orig, new)
     opcodes = matcher.get_opcodes()
@@ -27,7 +35,6 @@ def output_excel_formatting(orig: str, new: str, f_uline: Format, f_stkout: Form
             result.append(f_uline)
             result.append(new[j1:j2])
 
-    result.append(wr)
     return result
 
 
@@ -37,17 +44,19 @@ def output_to_excel_worksheet(diff_list: list, ofilename: str) -> None:
     worksheet = workbook.add_worksheet()
     worksheet.set_column(first_col=0, last_col=2, width=40)
     # Set up some formats to use.
-    normal = workbook.add_format()
-    underline = workbook.add_format({'underline': True, 'font_color': 'green'})
-    strikeout = workbook.add_format({'font_strikeout': True, 'font_color': 'red'})
-    wrap = workbook.add_format()
-    wrap.set_text_wrap()
+    normal = workbook.add_format({'text_wrap': True})
+    underline = workbook.add_format({'underline': True, 'font_color': 'green', 'text_wrap': True})
+    strikeout = workbook.add_format({'font_strikeout': True, 'font_color': 'red', 'text_wrap': True})
 
     for (s1, s2), row in zip(diff_list, range(len(diff_list))):
-        worksheet.write(row, 0, s1, wrap)
-        worksheet.write(row, 1, s2, wrap)
-        diff = output_excel_formatting(s1, s2, underline, strikeout, normal, wrap)
-        worksheet.write_rich_string(row, 2, *diff)
+        worksheet.write(row, 0, s1, normal)
+        worksheet.write(row, 1, s2, normal)
+        diff = output_excel_formatting(s1, s2, underline, strikeout, normal)
+
+        if len(diff) < 3:  # Either no change, or didn't exist in original version.
+            worksheet.write(row, 2, diff[1], diff[0])
+        else:
+            worksheet.write_rich_string(row, 2, *diff)
 
     workbook.close()
 
--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from diff_writer import output_to_excel_worksheet
 from pprint import pp
 
 ifile = input("Enter name of excel file to diff: ")
-wb = load_workbook(ifile) if ifile else load_workbook("test.xlsx")
+
+if not ifile:
+    ifile = 'test.xlsx'
+elif ifile[-5:] != ".xlsx":
+    ifile = f"{ifile}.xlsx"
+
 print(f"Running diff on {ifile}") if ifile else print(f"Running diff on test.xlsx since no filename specified...\n")
+wb = load_workbook(ifile)
 ws = wb.active
 
 row_list = []
@@ -13,12 +19,13 @@ for row in ws.values:
     for cell in row:
         value_list.append(cell)
     if len(value_list) != 2:
-        raise ValueError("Can only diff a two-column spreadsheet. Please update spreadsheet and be sure to delete extra whitespace.")
+        raise ValueError("Can only diff a two-column spreadsheet. "
+                         "Please update spreadsheet and be sure to delete extra whitespace.")
     else:
         row_list.append(value_list)
 
-print("List of strings to diff:")
-pp(row_list)
+# print("List of strings to diff:")
+# pp(row_list)
 
 print("Writing diff output to diff.xlsx")
 output_to_excel_worksheet(row_list, "diff.xlsx")
