772e8f7c09331d8a357e75a630754cb366157718
Jason Soares <jason.soares@viasat.com>
2025-01-30 08:19:29 -0800

    Refactored the code into a single class: DiffWriter
    Remove excel_merger.py, as this functionality is moved into the class.
    Simplified main.py to just call DiffWriter
    diff_writer.py: Moved all functionality into DiffWriter class. Improved class so it can handle multiple columns of data.

--- diff_writer.py
+++ diff_writer.py
@@ -1,91 +1,161 @@
 import xlsxwriter
 from xlsxwriter.worksheet import Format
 from difflib import SequenceMatcher
+import pandas as pd
 
 
-def output_excel_formatting(orig: str, new: str, f_uline: Format, f_stkout: Format, f_norm: Format) -> list:
+class DiffWriter:
     """
-    Compares two strings, orig and new. Returns a result as a list of formats and string slices that will tell
-    the calling function how to format the resulting text in Excel.
-    :param orig:
-    :param new:
-    :param f_uline:
-    :param f_stkout:
-    :param f_norm:
-    :return result: A variable length list of alternating Excel formats and string slices describing the redline
-    text, which can be either normal (no change), underline (all new), strikeout (all deleted), or a longer list
-    of alternating formats and string slices multiple insertions and deletions.
+    Takes two Excel files with the following columns, and creates a new excel file with the old
+    and new data. Input Excel files must have the following columns:
+    ID: Jama ID
+    Description: Requirement description text
+    Clarifying Information: Requirement clarifying information text
     """
-    if orig is None:  # New item that didn't exist in orig. Use underline style.
-        return [f_uline, new]
-    elif orig == new:  # No change since orig, use normal style.
-        return [f_norm, orig]
-    elif new is None:  # Completely deleted orig, use strikeout style.
-        return [f_stkout, orig]
+    def __init__(self, orig_fname: str, updated_fname: str, dest_fname: str):
+        self.orig_fname = orig_fname  # Excel file with original requirement data
+        self.updated_fname = updated_fname  # Excel file with modified requirement data
+        self.dest_fname = dest_fname  # Output Excel file
+        (self.workbook,
+         self.worksheet,
+         self.normal,
+         self.underline,
+         self.strikeout) = self.create_excel_worksheet_and_formats()  # Open workbook and create formats
+        self.df = self.compare_spreadsheets()  # Create a dataframe with orig, updated, orig -> updated redline data.
+        self.df.fillna("", inplace=True)  # Replace NaN with empty strings
+        print("Created dataframe from orig/dest requirement data.")
+        self.add_redline_column_to_df("Description")
+        self.add_redline_column_to_df("Clarifying Information")
+        print("Done creating redlines for requirement data.")
+        self.output_to_excel()
+        print("Done writing output to Excel.")
 
-    # Matcher gets a sequence of "opcodes" which say to insert, delete, etc. blocks of text.
-    matcher = SequenceMatcher(None, orig, new)
-    opcodes = matcher.get_opcodes()
+    def create_excel_worksheet_and_formats(self):
+        """
+        Create a workbook, add a worksheet to the workbook, and add basic formats representing normal, inserted, and
+        deleted text.
+        :return: Workbook, Worksheet, Formats
+        """
+        # Open a workbook and choose the column to format
+        workbook = xlsxwriter.Workbook(self.dest_fname)
+        worksheet = workbook.add_worksheet()
+        worksheet.set_column(first_col=0, last_col=6, width=50)
+        # Set up some formats to use.
+        normal = workbook.add_format({'text_wrap': True})
+        underline = workbook.add_format({'underline': True, 'font_color': 'green', 'text_wrap': True})
+        strikeout = workbook.add_format({'font_strikeout': True, 'font_color': 'red', 'text_wrap': True})
 
-    result = []
-    for tag, i1, i2, j1, j2 in opcodes:
-        if tag == 'equal':
-            result.append(f_norm)
-            result.append(orig[i1:i2])  # Insert the old text w/ plain formatting
-        elif tag == 'insert':
-            result.append(f_uline)
-            result.append(new[j1:j2])  # Insert the new text w/ underlined green formatting
-        elif tag == 'delete':
-            if orig[i1:i2] != "\n":
-                result.append(f_stkout)
-                result.append(orig[i1:i2].replace("\n", ""))  # Insert the old text with strikeout red.
-        elif tag == 'replace':
-            if orig[i1:i2] != "\n":
-                result.append(f_stkout)
-                result.append(orig[i1:i2].replace("\n", ""))  # Insert old text with strikeout red...
-            result.append(f_uline)
-            result.append(new[j1:j2])  # ...then insert new text with underline green.
+        return workbook, worksheet, normal, underline, strikeout
 
-    return result
+    def compare_spreadsheets(self) -> pd.DataFrame:
+        """
+        Takes in two spreadsheets, each with three fields: ID, Description, and Clarifying Information.
+        Output is a dictionary that is a merged version of the spreadsheets indexed to the updated file that
+        has the contents of the description and clarifying information side by side for comparison in the redline tool.
 
+        :return: Dataframe containing the merged spreadsheet data.
+        """
 
-def output_to_excel_worksheet(diff_list: list, ofilename: str) -> None:
-    """
-    Creates an Excel workbook and adds a worksheet to the workbook, adding some basic formatting to the columns.
-    Creates styles for new text and deleted text, then passes those along with the diff_list to
-    output_excel_formatting, which will output string A, B, and a diff that looks like a redline from A to B.
-    Finally, it writes all of the [A, B, Redline] items to the workbook.
+        # Read the original and updated spreadsheets
+        original_df = pd.read_excel(self.orig_fname, engine='openpyxl')
+        updated_df = pd.read_excel(self.updated_fname, engine='openpyxl')
+        updated_df.reset_index(inplace=True)
 
-    :param diff_list: A list of [A, B] strings to compare to each other.
-    :param ofilename: Output file name.
-    :return: None
-    """
-    # Open a workbook and choose the column to format
-    workbook = xlsxwriter.Workbook(ofilename)
-    worksheet = workbook.add_worksheet()
-    worksheet.set_column(first_col=0, last_col=2, width=40)
-    # Set up some formats to use.
-    normal = workbook.add_format({'text_wrap': True})
-    underline = workbook.add_format({'underline': True, 'font_color': 'green', 'text_wrap': True})
-    strikeout = workbook.add_format({'font_strikeout': True, 'font_color': 'red', 'text_wrap': True})
+        # Merge the dataframes on the ID column
+        merged_df = pd.merge(original_df, updated_df, on='ID', how='outer', suffixes=('_old', '_new'))
+        merged_df['index'] = merged_df['index'].fillna(len(merged_df))
 
-    for (s1, s2), row in zip(diff_list, range(len(diff_list))):
-        worksheet.write(row, 0, s1, normal)  # Write the original text to column A
-        worksheet.write(row, 1, s2, normal)  # write the updated text to column B
-        diff = output_excel_formatting(s1, s2, underline, strikeout, normal)  # Generate the formatted redline list.
+        # Sort the merged dataframe by the original order
+        merged_df.sort_values(by='index', inplace=True)
+        return merged_df
 
-        if len(diff) < 3:  # Either no change, or didn't exist in original version. Just reinsert old text for both.
-            worksheet.write(row, 2, diff[1], diff[0])  # diff[0] is probably always 'normal'
-        else:  # Write the redline text to col C with formats as enumerated in list diff.
-            worksheet.write_rich_string(row, 2, *diff)
+    def add_redline_column_to_df(self, column_name: str) -> None:
+        """
+        Apply the create_redline to each row in self.df and create a new column with the results for the given column.
+        :param column_name: The name of the column to be compared. Must have _old and _new suffixes.
+        :return: None
+        """
+        self.df[f"{column_name}_redline"] = self.df.apply(lambda row: self.create_redline(row[f"{column_name}_old"], row[f"{column_name}_new"]), axis=1)
 
-    workbook.close()
+    def create_redline(self, orig: str, new: str) -> list:
+        """
+        Compares two strings, orig and new. Returns a result as a list of formats and string slices that will tell
+        the calling function how to format the resulting text in Excel.
+        :param orig: Name of column with old data.
+        :param new: Name of column with new data.
+        :return result: A variable length list of alternating Excel formats and string slices describing the redline
+        text, which can be either normal (no change), underline (all new), strikeout (all deleted), or a longer list
+        of alternating formats and string slices representing multiple insertions and deletions.
+        """
+        if orig is None:  # New item that didn't exist in orig. Use underline style.
+            return [self.underline, new]
+        elif orig == new:  # No change since orig, use normal style.
+            return [self.normal, orig]
+        elif new is None:  # Completely deleted orig, use strikeout style.
+            return [self.strikeout, orig]
+
+        # Matcher gets a sequence of "opcodes" which say to insert, delete, etc. blocks of text.
+        matcher = SequenceMatcher(None, orig, new)
+        opcodes = matcher.get_opcodes()
+
+        result = []
+        for tag, i1, i2, j1, j2 in opcodes:
+            if tag == 'equal':
+                result.append(self.normal)
+                result.append(orig[i1:i2])  # Insert the old text w/ plain formatting
+            elif tag == 'insert':
+                result.append(self.underline)
+                result.append(new[j1:j2])  # Insert the new text w/ underlined green formatting
+            elif tag == 'delete':
+                if orig[i1:i2] != "\n":
+                    result.append(self.strikeout)
+                    result.append(orig[i1:i2].replace("\n", ""))  # Insert the old text with strikeout red.
+            elif tag == 'replace':
+                if orig[i1:i2] != "\n":
+                    result.append(self.strikeout)
+                    result.append(orig[i1:i2].replace("\n", ""))  # Insert old text with strikeout red...
+                result.append(self.underline)
+                result.append(new[j1:j2])  # ...then insert new text with underline green.
+
+        return result
+
+    def output_to_excel(self):
+        """
+            Creates an Excel workbook and adds a worksheet to the workbook, adding some basic formatting to the columns.
+            Creates styles for new text and deleted text, then passes those along with the diff_list to
+            output_excel_formatting, which will output string A, B, and a diff that looks like a redline from A to B.
+            Finally, it writes all the [A, B, Redline] items to the workbook.
+
+            :param diff_list: A list of [A, B] strings to compare to each other.
+            :param ofilename: Output file name.
+            :return: None
+            """
+
+        headings = ["ID",
+                    "Description_old",
+                    "Description_new",
+                    "Description_redline",
+                    "Clarifying Information_old",
+                    "Clarifying Information_new",
+                    "Clarifying Information_redline",
+                    ]
+
+        for inx, row in enumerate(self.df.to_dict(orient='records')):
+            for jnx, heading in enumerate(headings):
+                if "redline" in heading:
+                    if row[heading] and len(row[heading]) > 2:
+                        self.worksheet.write_rich_string(inx, jnx, *row[heading])
+                    elif row[heading] and len(row[heading]) == 2:
+                        if not row[headings[jnx-2]]:  # Old description/CI doesn't exist.
+                            self.worksheet.write(inx, jnx, row[headings[jnx-1]], self.underline)  # Write new text.
+                        if not row[headings[jnx-1]]:
+                            self.worksheet.write(inx, jnx, row[headings[jnx-2]], self.strikeout)  # Write deleted txt.
+                    else:
+                        self.worksheet.write(inx, jnx, "", self.normal)  # Write blank string to cell
+                else:
+                    self.worksheet.write(inx, jnx, row[heading], self.normal)
+        self.workbook.close()
 
 
 if __name__ == '__main__':
-    string_a = "The quick brown fox jumped over the lazy dog"
-    string_b = "The slow brown fox walked over a lazy cat"
-    string_c = "How now brown cow"
-    string_d = "How then brown hen"
-
-    output_to_excel_worksheet([[string_c, string_d], [string_a, string_b]])
+    diff = DiffWriter("original.xlsx", "updated.xlsx", "output.xlsx")
--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-from openpyxl import load_workbook
-from diff_writer import output_to_excel_worksheet
-from pprint import pp
+from diff_writer import DiffWriter
 
-ifile = input("Enter name of excel file to diff: ")
+OLD_DATA_EXCEL_FNAME = "original.xlsx"
+NEW_DATA_EXCEL_FNAME = "updated.xlsx"
 
-if not ifile:
-    ifile = 'test.xlsx'
-elif ifile[-5:] != ".xlsx":
-    ifile = f"{ifile}.xlsx"
 
-print(f"Running diff on {ifile}") if ifile else print(f"Running diff on test.xlsx since no filename specified...\n")
-wb = load_workbook(ifile)
-ws = wb.active
-
-# Walk through a spreadsheet with exactly two columns of data, and create a list of lists of the form:
-# [cell A string, cell B string] for comparison in the diff_writer.output_to_excel_worksheet funciton.
-row_list = []
-for row in ws.values:
-    value_list = []
-    for cell in row:
-        if cell and "\n\n" in cell:  # Removes annoying extra carriage returns in Jama exports.
-            value_list.append(cell.replace("\n\n", "\n"))
-        else:
-            value_list.append(cell)
-    if len(value_list) != 2:
-        raise ValueError("Can only diff a two-column spreadsheet. "
-                         "Please update spreadsheet and be sure to delete extra whitespace.")
-    else:
-        row_list.append(value_list)
-
-# print("List of strings to diff:")
-# pp(row_list)
-
-print("Writing diff output to diff.xlsx")
-output_to_excel_worksheet(row_list, "diff.xlsx")
+if __name__ == "__main__":
+    diff = DiffWriter(OLD_DATA_EXCEL_FNAME, NEW_DATA_EXCEL_FNAME, 'output.xlsx')
